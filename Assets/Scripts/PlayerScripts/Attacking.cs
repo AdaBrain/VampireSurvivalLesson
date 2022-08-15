632b774836b47d9de46ee90971c0cb1fd73d668a
adadesions <adadesions@gmail.com>
2022-08-15 16:51:01 +0700

    Init project

---
+++ Assets/Scripts/PlayerScripts/Attacking.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacking : MonoBehaviour
+{
+    [SerializeField] float cooldown = 0.5f;
+    [SerializeField] GameObject skillPrefab;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(AutoAttack());
+    }
+
+    private IEnumerator AutoAttack()
+    {        
+        while(true)
+        {
+            anim.SetTrigger("IsSpecial");
+            UseSkill();
+            yield return new WaitForSeconds(cooldown);
+        }
+    }
+
+    private void UseSkill()
+    {
+        GameObject skillClone = Instantiate(skillPrefab, transform.position, Quaternion.identity);
+
+        Destroy(skillClone, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 4.0f;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    public Animator Anim { get => anim; set => anim = value; }
+    public SpriteRenderer Sr { get => sr; set => sr = value; }
+    public Rigidbody2D Rb2d { get => rb2d; set => rb2d = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Movements();
+    }
+
+    private void Movements()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        if (hAxis != 0 || vAxis != 0)
+        {
+            Vector2 movement = new Vector2(hAxis, vAxis);
+            movement = rb2d.position + movement * speed * Time.fixedDeltaTime;
+
+            FlipSR(hAxis);
+            anim.SetBool("IsWalking", true);
+            rb2d.MovePosition(movement);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+
+    private void FlipSR(float hAxis)
+    {
+        sr.flipX = hAxis < 0;
+    }
+}
