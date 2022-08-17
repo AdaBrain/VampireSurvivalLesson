40ddb322b0cb91943bf4a7f176864f6831dbd363
adadesions <adadesions@gmail.com>
2022-08-17 15:37:42 +0700

    Hp bar can decrease from right to left

--- Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class PlayerHealth : MonoBehaviour, IHealth
     [SerializeField] private float health = 100.0f;
     [SerializeField] private float maxHealth = 100.0f;
     [SerializeField] private bool isDie = false;
+    [SerializeField] private RectTransform HPBar;
 
     // Properties
     public float Health {
@@ -24,6 +26,20 @@ public class PlayerHealth : MonoBehaviour, IHealth
     
 
     // Methods
+    void Update()
+    {
+        SetHPBar();
+    }
+
+    private void SetHPBar()
+    {
+        float deltaX = (Health / MaxHealth) - 1;
+        if (deltaX > 0) return;
+
+        Vector3 curPos = HPBar.localPosition;
+        HPBar.localPosition = new Vector3(deltaX, curPos.y, curPos.z);
+    }
+
     public void TakeDamage(float damage)
     {
         Health -= damage;       
